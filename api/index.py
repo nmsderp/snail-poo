6f1343b49190537d9a2dc3cd8da48d8cc0c6183d
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-01-26 21:18:54 -0500

    test

--- api/index.py
+++ api/index.py
@@ -1,20 +1,11 @@
-# app.py
-from flask import Flask, jsonify
-import requests
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def fetch_and_display_text():
-    txt_url = "https://editor.snail-ide.com/poo.txt"
+def home():
+    return 'Hello, World!'
 
-    try:
-        response = requests.get(txt_url)
-        response.raise_for_status()  # Raise an exception for bad responses
-        text_content = response.text
-        return jsonify({"text_content": text_content})
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/about')
+def about():
+    return 'About'
